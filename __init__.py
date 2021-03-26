3ee2e4001b14af3f9693002f02011760091fe892
Azin Zahraei <azin.zahraei@deepl.com>
2021-03-27 00:43:57 +0100

    add print

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ class Stopwatch(MycroftSkill):
 
     @intent_file_handler('stopwatch.intent')
     def handle_stopwatch(self, message):
+        print(message)
         self.speak_dialog('stopwatch')
 
 
