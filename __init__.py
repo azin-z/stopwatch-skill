7fb409768bf1d0c5da4359a5c565c730397feda0
Azin Zahraei <azin.zahraei@deepl.com>
2021-03-27 01:37:46 +0100

    adn

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import time
 
 class Stopwatch(MycroftSkill):
     def __init__(self):
+        self.starttime = None 
         MycroftSkill.__init__(self)
 
     @intent_file_handler('stopwatch.intent')
     def handle_stopwatch(self, message):
+        self.starttime = time.time()
         self.log.info(message)
         self.log.info("got a hang of logging")
 
@@ -15,6 +17,9 @@ class Stopwatch(MycroftSkill):
     @intent_file_handler('stopstopwatch.intent')
     def handle_stopwatch_stop(self, message):
         self.log.info("stopping stopwatch")
+        duration = time.time() - self.starttime
+        self.log.info(duration)
+        self.speak("stopwatch duration is {}".format(duration))
 
 def create_skill():
     return Stopwatch()
