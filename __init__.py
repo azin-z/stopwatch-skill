bf0621b4fe5f3063bad72d803a22d43f247ebd8c
Azin Zahraei <azin.zahraei@deepl.com>
2021-03-28 23:11:21 +0200

    Add update with timer

--- __init__.py
+++ __init__.py
@@ -1,25 +1,27 @@
 from mycroft import MycroftSkill, intent_file_handler
 import time
+from threading import Timer
 
 class Stopwatch(MycroftSkill):
     def __init__(self):
         self.starttime = None 
         MycroftSkill.__init__(self)
+    
+    def printStopwatchUpdate(self, currentTime):
+        self.speak('stopwatch has been running for {} seconds'.format(currentTime-self.starttime))
 
     @intent_file_handler('stopwatch.intent')
     def handle_stopwatch(self, message):
         self.starttime = time.time()
-        self.log.info(message)
-        self.log.info("got a hang of logging")
-
         self.speak_dialog('stopwatch')
+        Timer(1.0, printStopwatchUpdate, (time.time()))
 
     @intent_file_handler('stopstopwatch.intent')
     def handle_stopwatch_stop(self, message):
         self.log.info("stopping stopwatch")
         duration = time.time() - self.starttime
         self.log.info(duration)
-        self.speak("stopwatch duration is {}".format(duration))
+        self.speak("stopwatch duration is {}".format(round(duration))
 
 def create_skill():
     return Stopwatch()
